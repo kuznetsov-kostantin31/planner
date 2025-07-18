44763839f7d7a656e059e4cfeec261d4e33b483c
Konst <kuznetsov-kostantin31>
2025-07-18 19:02:09 +1000

    add auth and user service, edit prisma

---
+++ backend/src/auth/jwt.strategy.ts
@@ -0,0 +1,22 @@
+import { ConfigService } from '@nestjs/config'
+import { PassportStrategy } from '@nestjs/passport'
+
+import { ExtractJwt, Strategy } from 'passport-jwt'
+import { UserService } from 'src/user/user.service'
+
+export class JwtStrategy extends PassportStrategy(Strategy) {
+	constructor(
+		private configService: ConfigService,
+		private userService: UserService
+	) {
+		super({
+			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+			ignoreExpiration: true,
+			secretOrKey: configService.get('JWT_SECRET')
+		})
+	}
+
+	async validate({ id }: { id: string }) {
+		return this.userService.getById(id)
+	}
+}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,18 +1,22 @@
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import * as cookieParser from 'cookie-parser'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
+	const config = app.get(ConfigService)
+	const PORT = config.get('PORT')
+	const ORIGIN: string[] | any = config.get('ORIGIN')
 
 	app.setGlobalPrefix('api')
 	app.use(cookieParser())
 	app.enableCors({
-		origin: [process.env.ORIGIN],
+		origin: ORIGIN,
 		credentials: true,
 		exposedHeaders: 'set-cookie'
 	})
 
-	await app.listen(process.env.PORT ?? 4200)
+	await app.listen(PORT)
 }
 bootstrap()
